e1aa34e0c802899970b39b0ca9a86c369df08268
Brett Hazen <brett@basho.com>
2015-03-11 17:09:16 -0600

    Change default box to phusion instead of chef

--- provisioning/roles/erlang/files/build_otp.sh
+++ provisioning/roles/erlang/files/build_otp.sh
@@ -2,6 +2,7 @@
 OPENSSL=/usr/local/opt/openssl
 KERL=/usr/local/kerl/kerl
 INSTALLDIR=${HOME}/erlang
+ERLN8=/usr/local/erln8/bin/erln8
 export KERL_CONFIGURE_OPTIONS="--enable-smp-support --without-odbc --with-ssl=${OPENSSL} --enable-hipe --enable-m64-build"
 export CFLAGS="-g -O2"
 export LDFLAGS="-g"
@@ -31,8 +32,8 @@ export R16B02=${INSTALLDIR}/${DEFAULT}
 export CURRENT_OTP=${INSTALLDIR}/${DEFAULT}
 EOF
 fi
-if [ -n "`which erln8`" ]; then
-    erln8 --id=R15B01-basho --link ${INSTALLDIR}/R15B01-basho
-    erln8 --id=${DEFAULT} --link ${INSTALLDIR}/${DEFAULT}
+if [ -f "${ERLN8}" ]; then
+    ${ERLN8} --id=R15B01-basho --link ${INSTALLDIR}/R15B01-basho
+    ${ERLN8} --id=${DEFAULT} --link ${INSTALLDIR}/${DEFAULT}
     sed -i.bak "s/^system_default=$/system_default=${DEFAULT}/g" ${HOME}/.erln8.d/config
 fi
