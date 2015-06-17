1bad097e64657b8f23466e696d1fd15ceef26ec8
Brett Hazen <brett@basho.com>
2015-06-17 09:50:48 -0600

    Add support for CentOS 6.5

--- provisioning/roles/jdk/files/update_environment.sh
+++ provisioning/roles/jdk/files/update_environment.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 PROFILE=~/.bash_profile
-EXISTS=`grep java-7-oracle ${PROFILE}`
+if [ "$1" == "rhel" ]; then
+  JAVA_HOME=/usr/java/jdk1.7.0_79
+else
+  JAVA_HOME=/usr/lib/jvm/java-7-oracle
+fi
+EXISTS=`grep jdk ${PROFILE}`
 if [ -z "$EXISTS" ]; then
-   cat <<EOF >> ${PROFILE}
-export PATH=/usr/lib/jvm/java-7-oracle/bin:\${PATH}
+  cat <<EOF >> ${PROFILE}
+export JAVA_HOME=${JAVA_HOME}
+export PATH=\${JAVA_HOME}/bin:\${PATH}
 EOF
 fi
