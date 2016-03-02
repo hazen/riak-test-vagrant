92e512d374d53f6c2f429aeb33f0093fc75e9177
Brett Hazen <brett@basho.com>
2016-03-01 22:20:53 -0700

    Bump Erlang to R16B02-basho10

--- provisioning/roles/erlang/files/build_otp.sh
+++ provisioning/roles/erlang/files/build_otp.sh
@@ -13,14 +13,14 @@ export MAKE="make -j8"
 if [ -f /etc/centos-release ]; then
     export CFLAGS="-g -O2 -DOPENSSL_NO_EC=1"
 fi
-for release in R15B01-basho R16B02-basho9 18.1.3; do
+for release in R15B01-basho R16B02-basho10 18.1.3; do
     if [ -z "`${KERL} list builds | grep ${release}`" ]; then
          if [ "${release}" == "R15B01-basho" ]; then
              GIT_LABEL="basho_OTP_R15B01p"
          elif [ "${release}" == "18.1.3" ]; then
              GIT_LABEL="OTP-18.1.3"
          else
-             GIT_LABEL="OTP_R16B02_basho9"
+             GIT_LABEL="OTP_R16B02_basho10"
          fi
          ${KERL} build git git://github.com/basho/otp.git ${GIT_LABEL} ${release}
 #        KERL_CONFIGURE_OPTIONS="--enable-vm-probes --with-dynamic-trace=dtrace --enable-smp-support --without-odbc --with-ssl=/usr/local/opt/openssl --with-javac=/usr/lib/jvm/java-7-oracle/bin --enable-hipe" ${KERL}/${KERL} build ${release} ${release}
